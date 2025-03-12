32763f7fcdbee5cda30a1d56cf38c88dd3b94c1e
Dkanee <moussagb43@gmail.com>
2025-03-12 14:31:36 +0100

    exo11

--- introduction/App.js
+++ introduction/App.js
@@ -1,11 +1,12 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { FlexDemo } from "./components/FlexDemo/FlexDemo";
+import { AgeCounter } from "./components/AgeCounter/AgeCounter";
 
 export default function App() {
-  return (
+  return (  
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <FlexDemo />
+        <AgeCounter />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ introduction/components/AgeCounter/AgeCounter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+export function AgeCounter() {
+  const [age, setAge] = useState(30);
+
+  function increaseAge() {
+    setAge(prevAge => prevAge + 1);
+  }
+
+  return (
+    <>
+      <TouchableOpacity onPress={increaseAge}>
+        <Text style={{ fontSize: 40, color:"white",backgroundColor : "blue", borderRadius: 20, textAlign: "center" }}>Augmenter</Text>
+      </TouchableOpacity>
+      <Text style={{ fontSize: 40, color:"black", textAlign: "center" }}>J'ai {age} ans</Text>
+    </>
+  );
+}
