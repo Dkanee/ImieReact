b389669e99df7a3bf2e2717fc988291b44b72c77
Dkanee <moussagb43@gmail.com>
2025-03-12 14:10:56 +0100

    a

--- introduction/App.js
+++ introduction/App.js
@@ -1,49 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { Human } from "./components/Human/Human";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { sty } from "./css/app.style";
+import { FlexDemo } from "./components/FlexDemo/FlexDemo";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={[sty.square, { flex: 1 }]}>
-        <Text
-          style={{
-            color: "red",
-            backgroundColor: "#3300ee",
-            fontSize: 18,
-            padding: 5,
-          }}
-        >
-          Hello !
-        </Text>
-        <Human
-          lastName={"John"}
-          firstName={"Smith"}
-          age={30}
-          car={{ brand: "Citroen", maxSpeed: 180 }}
-          isHappy={true}
-          doSomething={function () {
-            console.log("Passage dans doSomeThing");
-          }}
-        >
-          <Image
-            style={{ height: 200, width: 300, marginTop: 10, borderRadius: 10 }}
-            source={{ uri: "https://picsum.photos/200/300" }}
-          />
-        </Human>
-        <Text>Hello !</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <FlexDemo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
---
+++ introduction/components/FlexDemo/FlexDemo.style.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+export const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  box1: {
+    backgroundColor: "red",
+    flex: 1,
+  },
+  box2: {
+    backgroundColor: "yellow",
+    flex: 1,
+  },
+  box3: {
+    backgroundColor: "red",
+    flex : 1,
+  },
+});
