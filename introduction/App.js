a99715e7da88b7f8ef5002aba4fd4684f6e1a82b
Dkanee <moussagb43@gmail.com>
2025-03-12 15:09:42 +0100

    exo13

--- introduction/App.js
+++ introduction/App.js
@@ -1,17 +1,30 @@
-import { Alert } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Child } from "./components/Child/Child";
+// import { Alert } from "react-native";
+// import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+// import { Child } from "./components/Child/Child";
 
-export default function App() {
-  function hello(name) {
-    Alert.alert(`Bonjour ${name} !`);
-  }
+import React from 'react';
+import { Text, View, Platform } from 'react-native';
 
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Child onPress={hello} />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{Platform.OS === 'ios' ? 'Je suis un IOS' : 'Je suis un Android'}</Text>
+    </View>
   );
-}
+};
+
+export default App;
+
+// export default function App() {
+//   function hello(name) {
+//     Alert.alert(`Bonjour ${name} !`);
+//   }
+
+//   return (
+//     <SafeAreaProvider>
+//       <SafeAreaView style={{ flex: 1 }}>
+//         <Child onPress={hello} />
+//       </SafeAreaView>
+//     </SafeAreaProvider>
+//   );
+// }
